0a457043e3e12426cfb2b7a159258313506036e2
reed17 <777reed777@gmail.com>
2019-03-19 16:22:08 +0200

    added join user to room test

--- src/test/java/com/impltech/chatApp/service/ChatRoomServiceUnitTest.java
+++ src/test/java/com/impltech/chatApp/service/ChatRoomServiceUnitTest.java
@@ -1,6 +1,7 @@
 package com.impltech.chatApp.service;
 
 import com.impltech.chatApp.dto.ChatRoomDto;
+import com.impltech.chatApp.dto.UserDto;
 import com.impltech.chatApp.entity.ChatRoom;
 import com.impltech.chatApp.entity.Message;
 import com.impltech.chatApp.entity.User;
@@ -16,12 +17,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.Answer;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.*;
 
 
@@ -96,5 +96,38 @@ public class ChatRoomServiceUnitTest {
     void whenGetAllExistedRoomsThenOperationIsSuccessful() {
         chatRoomService.getAllChatRoomsList();
         Mockito.verify(chatRoomRepository, times(1)).findAll();
+    }
+
+    @Test
+    void whenUsersJoinChatRoomThenOperationIsSuccessful() {
+        ChatRoomDto chatRoomDto = new ChatRoomDto();
+        chatRoomDto.setChatRoomId("1qaswed");
+        chatRoomDto.setName("joining room");
+        UserDto dto = new UserDto("test", "test@gmail.com", "1qazxsw2");
+
+        assertThat(chatRoomDto.getUsers().size(), is(0));
+
+        ChatRoom roomWithOneUser = new ChatRoom();
+        roomWithOneUser.setChatRoomId("1qaswed");
+        roomWithOneUser.setName("joining room");
+        User joiningUser = new User("test", "test@gmail.com", "1qazxsw2");
+
+        when(chatRoomRepository.findById(eq(roomWithOneUser.getChatRoomId()))).thenReturn(Optional.of(roomWithOneUser));
+        when(chatRoomRepository.save(roomWithOneUser))
+                .thenAnswer((Answer<ChatRoom>) invocation -> {
+                    ChatRoom room = invocation.getArgument(0);
+                    room.getConnectedUsers().add(0, joiningUser);
+                    room.getConnectedUsers().remove(1); // hardcoded now users size = 1
+                    return room;
+                });
+
+        chatRoomService.join(dto, chatRoomDto);
+
+        assertThat(roomWithOneUser.getConnectedUsers().size(), is(1));
+        verify(chatRoomRepository, times(1)).save(roomWithOneUser);
+        verify(simpMessagingTemplate, times(1)).convertAndSend(
+                destinationCaptor.capture(),
+                messageObjectCaptor.capture());
+        assertThat(roomWithOneUser.getConnectedUsers().contains(joiningUser), is(true));
     }
 }
